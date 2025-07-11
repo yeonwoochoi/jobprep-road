3f80a96097d36c46861072499b806580f443cbf8
beagle <chldusdn20@gmail.com>
2025-07-11 15:40:26 +0900

    feat: add container component

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,3 +1,24 @@
-export default function Container() {
-  return <div>Container</div>
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
+import clsx from "clsx";
+
+type ContainerProps<T extends ElementType> = {
+  as?: T
+  className?: string
+  children: ReactNode
+}
+
+export default function Container<T extends ElementType = 'div'>({
+  as,
+  className,
+  children,
+}: Omit<ComponentPropsWithoutRef<T>, keyof ContainerProps<T>> & ContainerProps<T>) {
+  let Component = as ?? 'div'
+
+  return (
+    <Component className={clsx('mx-auto max-w-7xl px-6 lg:px-8', className)}>
+      <div className="mx-auto max-w-2xl lg:max-w-none">
+        {children}
+      </div>
+    </Component>
+  )
 }
