46853be625393215c6ccb021ad5d114a236a39e8
beagle <chldusdn20@gmail.com>
2025-07-15 16:17:52 +0900

    chore: relax ESLint rules for unused vars and prefer-const to warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off', // any 타입 쓸수 있게
+      '@typescript-eslint/no-unused-vars': 'warn', // 사용하지 않는 변수 있을 때 경고만 표시
+      'prefer-const': 'warn', // 재할당 없는 변수는 const로 권장하는 규칙
+    }
+  }
 ];
 
 export default eslintConfig;
