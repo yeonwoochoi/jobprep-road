101187726b153dae60731fe7619aa5af5218c413
beagle <chldusdn20@gmail.com>
2025-07-24 13:05:07 +0900

    fix: modify return type null

--- src/actions/auth/login.action.ts
+++ src/actions/auth/login.action.ts
@@ -9,6 +9,6 @@ export const loginAction = createFormAction(
   async ({ email, password }) => {
     await delay(2000)
     console.log({ email, password })
-    return { email, password }
+    return null
   }
 )
